89cd4cc2def799b011ed8a62226e5e04c33bc138
olawale <olawaleisaac9@gmail.com>
2025-11-04 06:39:18 +0100

    Adding deep linking

--- src/navigation/NavContainer.tsx
+++ src/navigation/NavContainer.tsx
@@ -3,10 +3,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import RootNav from './RootNav';
 import { ZegoCallInvitationDialog } from '@zegocloud/zego-uikit-prebuilt-call-rn';
 import { navigationRef } from '../utils/navigation';
+import Login from '../screens/Auth/Login';
 
 const NavContainer = () => {
     return (
-        <NavigationContainer ref={navigationRef}>
+        <NavigationContainer
+            ref={navigationRef}
+            linking={{
+                prefixes: ['hairsap://', 'https://hairsap.com'],
+                config: { screens: { Login: '' } },
+            }}
+        >
             <ZegoCallInvitationDialog />
             <RootNav />
         </NavigationContainer>
